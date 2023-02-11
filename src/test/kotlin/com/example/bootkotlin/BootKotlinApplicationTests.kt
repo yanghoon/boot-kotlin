a6123db6d52d90f8a3586691557ad841e6b870bf
Hoon <yanghoon5@gmail.com>
2023-02-11 10:36:56 +0900

    Initial Commit

---
+++ src/test/kotlin/com/example/bootkotlin/BootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
